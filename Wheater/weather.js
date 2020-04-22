754935845687f2a983f3c90a66d3cf5ff85c7099
Felo302 <64108778+Felo302@users.noreply.github.com>
2020-04-22 11:45:52 -0400

    weather of city using API

---
+++ Wheater/weather.js
@@ -0,0 +1,27 @@
+﻿"use strict"
+
+const urlname = "http://api.openweathermap.org/data/2.5/weather?q=Milton&appid=ffb41b744b6eeaabf18134ae898a5d75";
+
+
+$.ajax ({
+    url: urlname,
+    success: function(result){
+        console.log(result);
+        console.log(result.name);
+        console.log(result.weather[0].description)
+        // display the temperature
+        let displaycity = `Current weather in ${result.name}`;
+        $("#location").html(displaycity);
+
+        let f = Math.round(result.main.temp * (9 / 50) - 459.67); // this varialble is not a string. so you have to change it to string.
+        let fahrenheit = f.toString();
+        $("#temp").text(fahrenheit);
+
+        let windSpeed = Math.round(result.wind.speed / .44704);// this varialble is not a string. so you have to change it to string.
+        let wind = windSpeed.toString();
+        $("#wind").text(wind);
+
+        let skydisplay = `Today's ${result.weather[0].description}`
+        $("#sky").html(skydisplay);
+    }
+})
